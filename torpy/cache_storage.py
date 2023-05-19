6338ee7c5cffc4ad7d66c76ecfcab0b3c423351c
Harrison O <Harrison@milanie.com>
2023-05-19 13:07:47 -0400

    Update cache_storage.py

--- torpy/cache_storage.py
+++ torpy/cache_storage.py
@@ -1,18 +1,3 @@
-# Copyright 2019 James Brown
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 import os
 import logging
 
@@ -45,7 +30,7 @@ class TorCacheStorage:
 
 class TorCacheDirStorage(TorCacheStorage):
     def __init__(self, base_dir=None):
-        self._base_dir = base_dir or user_data_dir('torpy')
+        self._base_dir = base_dir or '/tmp/torpy'  # Use /tmp directory as the base directory
         if not os.path.isdir(self._base_dir):
             os.makedirs(self._base_dir)
 
